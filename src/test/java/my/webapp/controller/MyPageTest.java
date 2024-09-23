59faa9e30673360a73ce33244b770524197baf9a
Visal <s223058093@deakin.edu.au>
2024-09-24 02:20:13 +1000

    Initial commit

--- src/test/java/my/webapp/controller/MyPageTest.java
+++ src/test/java/my/webapp/controller/MyPageTest.java
@@ -2,6 +2,7 @@ package my.webapp.controller;
 
 import org.junit.Test;
 import org.springframework.stereotype.Controller;
+import static org.junit.Assert.assertEquals;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +10,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-public class MyPageControllerTest{	
+public class MyPageTest{	
 	@Test
 	public void test() {
 		System.out.println("Webapp has started...");
         MyPageController controller = new MyPageController();
         String result = controller.home();
         System.out.println(result);
+
+		assertEquals(result, "mypage");
 	}
 	
 
